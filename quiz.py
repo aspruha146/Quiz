3436328ff0d9f0a7325e87e404cd5670d6355c42
aspruha146 <pihu.b2002@gmail.com>
2025-04-13 18:51:59 +0530

    first commit

---
+++ quiz.py
@@ -0,0 +1,41 @@
+print("welcome my quiz game")
+playing=input("Do you want to play? ")
+if playing != 'yes':
+   quit()
+print("okey Let's Play:)")
+answer =  input("what CPU stands for? ")
+if answer == "central processing unit":
+    print('correct!')
+else:
+    print('incorrect!')
+    
+
+answer =  input("Which type of Programming does Python support? ")
+if answer == "object oriented programming":
+    print('correct!')
+else:
+    print('incorrect!')
+    
+answer =  input(" Is Python code compiled or interpreted?")
+if answer == "both compiled and interpreted":
+    print('correct!')
+else:
+    print('incorrect!')
+    
+answer =  input("Which keyword is used for function in Python language?")
+if answer == "def":
+    print('correct!')
+else:
+    print('incorrect!')
+    
+answer =  input("Python supports the creation of anonymous functions at runtime, using a construct called ")
+if answer == "lambda":
+    print('correct!')
+else:
+    print('incorrect!')
+    
+answer =  input("What does pip stand for python?")
+if answer == "Preferred Installer Program":
+    print('correct!')
+else:
+    print('incorrect!')
